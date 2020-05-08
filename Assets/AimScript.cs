41aec5e43c4f68c6aaa4999b1617f6bf0e7997d5
saltire sable <saltiresable@gmail.com>
2020-05-08 16:03:08 -0400

    Initial commit with some test sprites and a rough aiming mechanism

---
+++ Assets/AimScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AimScript : MonoBehaviour {
+  float deadZone = 0.3f;
+
+  void FixedUpdate() {
+    Gamepad gamepad = Gamepad.current;
+
+    if (gamepad != null) {
+      Vector2 aim = gamepad.leftStick.ReadValue();
+      if (aim.magnitude >= deadZone) {
+        float angle = Vector2.SignedAngle(Vector2.up, aim);
+        transform.localRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+      }
+    }
+  }
+}
