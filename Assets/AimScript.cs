4fcf04a2b613cb90a0a71f20ea479857e996fc84
saltire sable <saltiresable@gmail.com>
2020-05-09 00:35:35 -0400

    Add some basic slashing

--- Assets/AimScript.cs
+++ Assets/AimScript.cs
@@ -4,16 +4,40 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class AimScript : MonoBehaviour {
-  float deadZone = 0.3f;
+  public Transform slashIndicator;
+  public float radius = 4;
+  public float deadZone = 0.3f;
+  public float slashDuration = .5f;
 
-  void FixedUpdate() {
+  float slashTimeRemaining = 0;
+  Vector2 slashStart;
+
+  void Update() {
     Gamepad gamepad = Gamepad.current;
+    if (gamepad == null) {
+      return;
+    }
 
-    if (gamepad != null) {
+    if (slashTimeRemaining <= 0) {
       Vector2 aim = gamepad.leftStick.ReadValue();
       if (aim.magnitude >= deadZone) {
         float angle = Vector2.SignedAngle(Vector2.up, aim);
         transform.localRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+      }
+
+      if (gamepad.rightTrigger.wasPressedThisFrame) {
+        slashTimeRemaining = slashDuration;
+        slashStart = aim.normalized * radius;
+        slashIndicator.gameObject.SetActive(true);
+      }
+    }
+
+    if (slashTimeRemaining > 0) {
+      slashIndicator.position = Vector2.Lerp(slashStart, -slashStart, (slashDuration - slashTimeRemaining) / slashDuration);
+      slashTimeRemaining -= Time.deltaTime;
+
+      if (slashTimeRemaining <= 0) {
+        slashIndicator.gameObject.SetActive(false);
       }
     }
   }
