330940da8402872e09810ac146e9ea8617c05711
saltire sable <saltiresable@gmail.com>
2020-05-09 14:09:10 -0400

    Adjust aiming, and draw a line for sword position

--- Assets/AimScript.cs
+++ Assets/AimScript.cs
@@ -5,12 +5,25 @@ using UnityEngine.InputSystem;
 
 public class AimScript : MonoBehaviour {
   public Transform slashIndicator;
+  public Transform aimIndicator;
+  public Sprite[] swordSprites;
+
   public float radius = 4;
+  public float angleRange = 120;
   public float deadZone = 0.3f;
   public float slashDuration = .5f;
 
+  public float swordAngleStart = -75;
+
+  float minAngle;
+  float maxAngle;
+  float angle;
+
+  float ellipseOffsetY = 2;
+  float ellipseRadiusX = 1;
+  float ellipseRadiusY = 3;
+
   float slashTimeRemaining = 0;
-  Vector2 slashStart;
 
   void Update() {
     Gamepad gamepad = Gamepad.current;
@@ -19,26 +32,42 @@ public class AimScript : MonoBehaviour {
     }
 
     if (slashTimeRemaining <= 0) {
-      Vector2 aim = gamepad.leftStick.ReadValue();
-      if (aim.magnitude >= deadZone) {
-        float angle = Vector2.SignedAngle(Vector2.up, aim);
-        transform.localRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+      slashIndicator.gameObject.SetActive(false);
+
+      Vector2 inputAim = gamepad.leftStick.ReadValue();
+      if (inputAim.magnitude >= deadZone) {
+        angle = Vector2.SignedAngle(Vector2.up, inputAim);
+        float minAngle = (180 - angleRange) / 2;
+        float maxAngle = 180 - minAngle;
+        angle = Mathf.Sign(angle) * Mathf.Clamp(Mathf.Abs(angle), minAngle, maxAngle);
+        aimIndicator.position = Quaternion.AngleAxis(angle, Vector3.forward) * Vector2.up * radius;
       }
 
       if (gamepad.rightTrigger.wasPressedThisFrame) {
         slashTimeRemaining = slashDuration;
-        slashStart = aim.normalized * radius;
         slashIndicator.gameObject.SetActive(true);
       }
     }
 
     if (slashTimeRemaining > 0) {
-      slashIndicator.position = Vector2.Lerp(slashStart, -slashStart, (slashDuration - slashTimeRemaining) / slashDuration);
+      float lerpValue = (slashDuration - slashTimeRemaining) / slashDuration;
       slashTimeRemaining -= Time.deltaTime;
 
-      if (slashTimeRemaining <= 0) {
-        slashIndicator.gameObject.SetActive(false);
-      }
+      slashIndicator.position = Vector2.Lerp(aimIndicator.position, -aimIndicator.position, lerpValue);
+
+      Vector2 ellipseCenter = Vector2.down * ellipseOffsetY;
+
+      float swordAngle = Mathf.Lerp(-swordAngleStart, swordAngleStart, lerpValue);
+      float swordAngleRad = (swordAngle + 90) * Mathf.Deg2Rad;
+      Vector2 handlePoint = ellipseCenter + new Vector2(ellipseRadiusY * Mathf.Cos(swordAngleRad), ellipseRadiusX * Mathf.Sin(swordAngleRad));
+      float swordHeight = -handlePoint.y;
+      float swordWidth = swordHeight * Mathf.Tan(-swordAngle * Mathf.Deg2Rad);
+      Vector2 swordPoint = handlePoint + new Vector2(swordWidth, swordHeight);
+
+      Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+      Debug.DrawLine(rotation * handlePoint, rotation * swordPoint);
+
+      // slashIndicator.GetComponent<SpriteRenderer>().sprite = swordSprites[(int)(lerpValue * swordSprites.Length)];
     }
   }
 }
