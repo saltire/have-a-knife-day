a1709d167cfb8faea3eb571eb6752cb2e5abfa5f
saltire sable <saltiresable@gmail.com>
2020-05-09 19:49:50 -0400

    Refactor sword script, import enemy sword sprites

--- Assets/AimScript.cs
+++ Assets/AimScript.cs
@@ -4,33 +4,15 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class AimScript : MonoBehaviour {
-  public Transform sword;
+  public SwordScript sword;
   public Transform aimIndicator;
-  public Sprite[] swordSprites;
 
   public float radius = 4;
   public float angleRange = 120;
   public float deadZone = 0.3f;
-  public float slashDuration = .5f;
-
-  public float swingAngleStart = -75;
-
-  SpriteRenderer swordSpriter;
-  TrailRenderer swordTrail;
 
   float aimAngle;
   float blockAngleModifier = 0.6f;
-
-  public float ellipseRadiusX = 3;
-  public float ellipseRadiusY = 1;
-  public float ellipseOffsetY = -2;
-
-  float slashTimeRemaining = 0;
-
-  void Start() {
-    swordSpriter = sword.GetComponent<SpriteRenderer>();
-    swordTrail = sword.GetComponentInChildren<TrailRenderer>();
-  }
 
   void Update() {
     Gamepad gamepad = Gamepad.current;
@@ -38,7 +20,7 @@ public class AimScript : MonoBehaviour {
       return;
     }
 
-    if (slashTimeRemaining <= 0) {
+    if (!sword.IsSlashing()) {
       Vector2 leftAim = gamepad.leftStick.ReadValue();
       Vector2 rightAim = gamepad.rightStick.ReadValue();
       if (leftAim.magnitude >= deadZone) {
@@ -51,26 +33,14 @@ public class AimScript : MonoBehaviour {
       if (gamepad.leftShoulder.isPressed || gamepad.rightShoulder.isPressed) {
         float blockAngle = (Mathf.Abs(aimAngle) * blockAngleModifier + angleRange * (1 - blockAngleModifier)) *
           (gamepad.leftShoulder.isPressed ? 1 : -1);
-        PositionSword(0, blockAngle);
-        sword.position -= new Vector3(sword.position.x, 0, 0);
-        sword.localScale *= new Vector2(-1, 1);
-        sword.localRotation = Quaternion.Euler(0, 0, -sword.localRotation.eulerAngles.z);
+        sword.Block(blockAngle);
       }
       else if (gamepad.leftTrigger.wasPressedThisFrame || gamepad.rightTrigger.wasPressedThisFrame) {
-        slashTimeRemaining = slashDuration;
-        swordTrail.emitting = true;
+        sword.StartSlashing(aimAngle);
       }
-    }
-
-    if (slashTimeRemaining > 0) {
-      float lerpValue = (slashDuration - slashTimeRemaining) / slashDuration;
-      PositionSword(lerpValue);
-
-      if (lerpValue > .9f) {
-        swordTrail.emitting = false;
+      else {
+        sword.PositionSword(0, aimAngle);
       }
-
-      slashTimeRemaining -= Time.deltaTime;
     }
   }
 
@@ -80,38 +50,5 @@ public class AimScript : MonoBehaviour {
     float maxAngle = 180 - minAngle;
     aimAngle = Mathf.Sign(aimAngle) * Mathf.Clamp(Mathf.Abs(aimAngle), minAngle, maxAngle);
     aimIndicator.position = Quaternion.AngleAxis(aimAngle, Vector3.forward) * Vector2.up * radius;
-    PositionSword(0);
-  }
-
-  void PositionSword(float lerpValue) {
-    PositionSword(lerpValue, aimAngle);
-  }
-
-  void PositionSword(float lerpValue, float angle) {
-    if (angle < 0) {
-      lerpValue = 1 - lerpValue;
-    }
-
-    float swingAngle = Util.EaseInOutCubic(-swingAngleStart, swingAngleStart, lerpValue);
-    float swingAngleRad = (swingAngle + 90) * Mathf.Deg2Rad;
-
-    Vector2 ellipseCenter = new Vector2(0, ellipseOffsetY);
-    Vector2 handlePoint = ellipseCenter + new Vector2(ellipseRadiusX * Mathf.Cos(swingAngleRad), ellipseRadiusY * Mathf.Sin(swingAngleRad));
-    float swordHeight = -handlePoint.y;
-    float swordWidth = swordHeight * Mathf.Tan(-swingAngle * Mathf.Deg2Rad);
-    Vector2 swordPoint = handlePoint + new Vector2(swordWidth, swordHeight);
-
-    Quaternion rotation = Quaternion.AngleAxis(angle - 90 * Mathf.Sign(angle), Vector3.forward);
-    handlePoint = rotation * handlePoint;
-    swordPoint = rotation * swordPoint;
-    // Debug.DrawLine(handlePoint, swordPoint);
-
-    // slashIndicator.position = Vector2.Lerp(aimIndicator.position, -aimIndicator.position, lerpValue);
-    sword.position = swordPoint;
-    sword.localRotation = rotation;
-
-    swordSpriter.sprite = swordSprites[Mathf.Min((int)(lerpValue * swordSprites.Length), swordSprites.Length - 1)];
-    float spriteHeight = swordSpriter.sprite.rect.height / swordSpriter.sprite.pixelsPerUnit;
-    sword.localScale = Vector2.one * swordHeight / spriteHeight;
   }
 }
---
+++ Assets/SwordScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordScript : MonoBehaviour {
+  public Sprite[] swordSprites;
+
+  public float slashDuration = .5f;
+
+  public float swingAngleStart = -75;
+
+  SpriteRenderer swordSpriter;
+  TrailRenderer swordTrail;
+
+  public float ellipseRadiusX = 3;
+  public float ellipseRadiusY = 1;
+  public float ellipseOffsetY = -2;
+
+  float slashTimeRemaining = 0;
+  float slashAngle = 0;
+
+  void Start() {
+    swordSpriter = GetComponent<SpriteRenderer>();
+    swordTrail = GetComponentInChildren<TrailRenderer>();
+  }
+
+  void Update() {
+    if (slashTimeRemaining > 0) {
+      float lerpValue = (slashDuration - slashTimeRemaining) / slashDuration;
+      PositionSword(lerpValue, slashAngle);
+
+      if (lerpValue > .9f) {
+        swordTrail.emitting = false;
+      }
+
+      slashTimeRemaining -= Time.deltaTime;
+    }
+  }
+
+  public void StartSlashing(float angle) {
+    slashAngle = angle;
+    slashTimeRemaining = slashDuration;
+    swordTrail.emitting = true;
+  }
+
+  public bool IsSlashing() {
+    return slashTimeRemaining > 0;
+  }
+
+  public void Block(float blockAngle) {
+    PositionSword(0, blockAngle);
+    transform.position -= new Vector3(transform.position.x, 0, 0);
+    transform.localScale *= new Vector2(-1, 1);
+    transform.localRotation = Quaternion.Euler(0, 0, -transform.localRotation.eulerAngles.z);
+  }
+
+  public void PositionSword(float lerpValue, float angle) {
+    if (angle < 0) {
+      lerpValue = 1 - lerpValue;
+    }
+
+    float swingAngle = Util.EaseInOutCubic(-swingAngleStart, swingAngleStart, lerpValue);
+    float swingAngleRad = (swingAngle + 90) * Mathf.Deg2Rad;
+
+    Vector2 ellipseCenter = new Vector2(0, ellipseOffsetY);
+    Vector2 handlePoint = ellipseCenter + new Vector2(ellipseRadiusX * Mathf.Cos(swingAngleRad), ellipseRadiusY * Mathf.Sin(swingAngleRad));
+    float swordHeight = -handlePoint.y;
+    float swordWidth = swordHeight * Mathf.Tan(-swingAngle * Mathf.Deg2Rad);
+    Vector2 swordPoint = handlePoint + new Vector2(swordWidth, swordHeight);
+
+    Quaternion rotation = Quaternion.AngleAxis(angle - 90 * Mathf.Sign(angle), Vector3.forward);
+    handlePoint = rotation * handlePoint;
+    swordPoint = rotation * swordPoint;
+    // Debug.DrawLine(handlePoint, swordPoint);
+
+    // slashIndicator.position = Vector2.Lerp(aimIndicator.position, -aimIndicator.position, lerpValue);
+    transform.position = swordPoint;
+    transform.localRotation = rotation;
+
+    swordSpriter.sprite = swordSprites[Mathf.Min((int)(lerpValue * swordSprites.Length), swordSprites.Length - 1)];
+    float spriteHeight = swordSpriter.sprite.rect.height / swordSpriter.sprite.pixelsPerUnit;
+    transform.localScale = Vector2.one * swordHeight / spriteHeight;
+  }
+}
