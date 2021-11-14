d61da6c44bc6cd62df9f8f6e9b9741da487d7d9f
saltire sable <saltiresable@gmail.com>
2021-11-14 15:22:55 -0500

    Merge input and sword scripts

--- Assets/2D/Sword.cs
+++ Assets/2D/Sword.cs
@@ -1,60 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Sword : MonoBehaviour {
-  // Angles are clockwise from the top.
-  // public float minStabAngle = 140;
-  // public float maxStabAngle = 220;
-
-  // public Vector2 stabOrigin;
-  // public float stabRadius;
+  public float scale = 5;
+  public float unitsPerMetre = 5;
+  public float moveDuration = .2f;
+  public float maxMoveSpeed = 10;
+  Vector3 moveVelocity;
 
   public Collider2D[] stabAreas;
 
-  // float velocity = 0;
-  Vector3 velocity = Vector3.zero;
-  public float aimDuration = .1f;
-  public float maxSpeed = 1000;
+  Vector3 rotateVelocity = Vector3.zero;
+  public float rotateDuration = .1f;
+  public float maxRotateSpeed = 1000;
 
   public Transform target;
 
   void Update() {
-    // 2D angle of the hand's position around the centre Z-axis, clockwise from the top.
-    float armAngle = Vector2.SignedAngle(transform.position, Vector3.down) + 180;
+    Gamepad gamepad = Gamepad.current;
+    if (gamepad == null) {
+      return;
+    }
 
-    // Aim the sword differently depending if the arm is at a slash or stab angle.
-    // bool aimForStab = (armAngle > minStabAngle && armAngle < maxStabAngle)
-    //   || Vector2.Distance(transform.position, stabOrigin) <= stabRadius;
-    bool aimForStab = stabAreas.Any(coll => coll.OverlapPoint(transform.position));
+    Vector2 rightStick = gamepad.rightStick.ReadValue();
 
-    // float currentAimAngle = transform.eulerAngles.z;
-
-    // // TODO: During a slash, continue using slash aim until slash is over.
-    // // Then go back to using either slash or stab aim depending on the arm angle.
-
-    // float targetAimAngle = stabAngle ? -(armAngle + 180) : -armAngle;
-
-    // float aimAngle = Mathf.SmoothDampAngle(
-    //   currentAimAngle, targetAimAngle,
-    //   ref velocity, aimDuration, maxSpeed);
-
-    // transform.rotation = Quaternion.Euler(0, 0, aimAngle);
-
+    // Move sword around the circle.
+    transform.position = Vector3.SmoothDamp(
+      transform.position, rightStick * scale, ref moveVelocity, moveDuration, maxMoveSpeed);
 
     Vector3 currentAimDir = transform.rotation * Vector3.up;
+
+    // Aim the sword differently depending if the arm is at a slash or stab angle.
+    bool aimForStab = stabAreas.Any(coll => coll.OverlapPoint(transform.position));
     Vector3 targetAimDir = aimForStab ? target.position - transform.position
       : transform.position - target.position;
 
-    // Vector3 axis = Vector3.Cross(currentAimDir, targetAimDir);
-    // float angle = Vector3.Angle(currentAimDir, targetAimDir);
-
-    // float moveAngle = Mathf.SmoothDampAngle(0, angle, ref velocity, aimDuration, maxSpeed);
-    // transform.RotateAround(transform.position, axis, moveAngle);
-
-    Vector3 aimDir = Vector3.SmoothDamp(currentAimDir, targetAimDir, ref velocity,
-      aimDuration, maxSpeed);
+    Vector3 aimDir = Vector3.SmoothDamp(currentAimDir, targetAimDir, ref rotateVelocity,
+      rotateDuration, maxRotateSpeed);
     transform.rotation = Quaternion.FromToRotation(Vector3.up, aimDir);
   }
 }
