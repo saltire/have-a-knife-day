7408b8f3582ee767f42f31db793b856144e67bcd
saltire sable <saltiresable@gmail.com>
2021-11-13 21:19:48 -0500

    Aim sword 3-dimensionally toward a target in front

--- Assets/2D/Sword.cs
+++ Assets/2D/Sword.cs
@@ -4,30 +4,51 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour {
   // Angles are clockwise from the top.
-  public float minStabAngle = 90;
-  public float maxStabAngle = 225;
+  public float minStabAngle = 140;
+  public float maxStabAngle = 220;
+
+  public Vector2 stabOrigin;
+  public float stabRadius;
 
   float velocity = 0;
+  float velocity2 = 0;
   public float aimDuration = .05f;
   public float maxSpeed = 1000;
 
+  public Transform target;
+
   void Update() {
+    // 2D angle of the hand's position around the centre Z-axis, clockwise from the top.
     float armAngle = Vector2.SignedAngle(transform.position, Vector3.down) + 180;
 
-    float currentAimAngle = transform.eulerAngles.z;
-
     // Aim the sword differently depending if the arm is at a slash or stab angle.
+    // bool aimForStab = armAngle > minStabAngle && armAngle < maxStabAngle;
+    bool aimForStab = Vector2.Distance(transform.position, stabOrigin) <= stabRadius;
 
-    // TODO: During a slash, continue using slash aim until slash is over.
-    // Then go back to using either slash or stab aim depending on the arm angle.
+    // float currentAimAngle = transform.eulerAngles.z;
 
-    float targetAimAngle = (armAngle > minStabAngle && armAngle < maxStabAngle)
-      ? -(armAngle + 180) : -armAngle;
+    // // TODO: During a slash, continue using slash aim until slash is over.
+    // // Then go back to using either slash or stab aim depending on the arm angle.
 
-    float aimAngle = Mathf.SmoothDampAngle(
-      currentAimAngle, targetAimAngle,
-      ref velocity, aimDuration, maxSpeed);
+    // float targetAimAngle = stabAngle ? -(armAngle + 180) : -armAngle;
 
-    transform.rotation = Quaternion.Euler(0, 0, aimAngle);
+    // float aimAngle = Mathf.SmoothDampAngle(
+    //   currentAimAngle, targetAimAngle,
+    //   ref velocity, aimDuration, maxSpeed);
+
+    // transform.rotation = Quaternion.Euler(0, 0, aimAngle);
+
+
+
+    Vector3 currentAimDir = transform.rotation * Vector3.up;
+    Vector3 targetAimDir = aimForStab ? target.position - transform.position
+      : transform.position - target.position;
+
+    Vector3 axis = Vector3.Cross(currentAimDir, targetAimDir);
+    float angle = Vector3.Angle(currentAimDir, targetAimDir);
+
+    float moveAngle = Mathf.SmoothDampAngle(0, angle, ref velocity2, aimDuration, maxSpeed);
+
+    transform.RotateAround(transform.position, axis, moveAngle);
   }
 }
