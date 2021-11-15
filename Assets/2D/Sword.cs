2d8ce26e3ad46dd3e7594f239aad27784333f634
saltire sable <saltiresable@gmail.com>
2021-11-14 19:04:38 -0500

    Start uninterruptible slash if sword enters slash area fast enough

--- Assets/2D/Sword.cs
+++ Assets/2D/Sword.cs
@@ -9,6 +9,7 @@ public class Sword : MonoBehaviour {
   public float maxMoveSpeed = 10;
   Vector3 moveVelocity;
 
+  public Collider2D slashArea;
   public Collider2D[] stabAreas;
 
   Vector3 rotateVelocity = Vector3.zero;
@@ -16,6 +17,13 @@ public class Sword : MonoBehaviour {
   public float maxRotateSpeed = 1000;
 
   public Transform target;
+
+  bool inSlashArea = false;
+  Vector3? slashTarget;
+  public float minSlashVelocity = 20;
+
+  public float slashCooldown = .25f;
+  float cooldownRemaining = 0;
 
   void Update() {
     Gamepad gamepad = Gamepad.current;
@@ -25,9 +33,21 @@ public class Sword : MonoBehaviour {
 
     Vector2 rightStick = gamepad.rightStick.ReadValue();
 
-    // Move sword around the circle.
-    transform.position = Vector3.SmoothDamp(
-      transform.position, rightStick * scale, ref moveVelocity, moveDuration, maxMoveSpeed);
+    if (!slashTarget.HasValue) {
+      // Move sword toward the stick direction.
+      transform.position = Vector3.SmoothDamp(
+        transform.position, rightStick * scale, ref moveVelocity, moveDuration, maxMoveSpeed);
+    }
+    else {
+      // Move the sword toward the slash target. TODO: different speed values.
+      transform.position = Vector3.SmoothDamp(
+        transform.position, slashTarget.Value, ref moveVelocity, moveDuration * 5, maxMoveSpeed);
+
+      if (Vector3.Distance(transform.position, slashTarget.Value) < .1f) {
+        slashTarget = null;
+        cooldownRemaining = slashCooldown;
+      }
+    }
 
     Vector3 currentAimDir = transform.rotation * Vector3.up;
 
@@ -39,5 +59,24 @@ public class Sword : MonoBehaviour {
     Vector3 aimDir = Vector3.SmoothDamp(currentAimDir, targetAimDir, ref rotateVelocity,
       rotateDuration, maxRotateSpeed);
     transform.rotation = Quaternion.FromToRotation(Vector3.up, aimDir);
+
+    if (cooldownRemaining > 0) {
+      cooldownRemaining -= Time.deltaTime;
+    }
+
+    if (!slashTarget.HasValue && cooldownRemaining <= 0) {
+      // If the sword is entering the slash area at a minimum speed, find and set the slash target.
+      bool wasInSlashArea = inSlashArea;
+      inSlashArea = slashArea.OverlapPoint(transform.position);
+
+      if (
+        !aimForStab && inSlashArea && !wasInSlashArea && moveVelocity.magnitude >= minSlashVelocity
+      ) {
+        // Find the target point on the opposite edge of the circle by passing it and going back.
+        RaycastHit2D hit = Physics2D.Raycast(
+          transform.position + moveVelocity.normalized * scale * 3, -moveVelocity);
+        slashTarget = hit.point;
+      }
+    }
   }
 }
