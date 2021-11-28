664fae5b19d8ecef1e4e132884112520d8d7a9df
saltire sable <saltiresable@gmail.com>
2021-11-28 16:21:05 -0500

    Improve sword angle, including while slashing

--- Assets/2D/Sword.cs
+++ Assets/2D/Sword.cs
@@ -12,18 +12,21 @@ public class Sword : MonoBehaviour {
   public Collider2D slashArea;
   public Collider2D[] stabAreas;
 
-  Vector3 rotateVelocity = Vector3.zero;
+  bool inSlashArea = false;
+  public float minSlashVelocity = 20;
+  public float slashCooldown = .25f;
+  float cooldownRemaining = 0;
+
+  bool slashing;
+  Vector3 slashStart;
+  Vector3 slashTarget;
+  float slashScale;
+
+  Quaternion rotateDeriv = Quaternion.identity;
   public float rotateDuration = .1f;
   public float maxRotateSpeed = 1000;
 
-  public Transform target;
-
-  bool inSlashArea = false;
-  Vector3? slashTarget;
-  public float minSlashVelocity = 20;
-
-  public float slashCooldown = .25f;
-  float cooldownRemaining = 0;
+  public Transform stabTarget;
 
   void Update() {
     Gamepad gamepad = Gamepad.current;
@@ -32,8 +35,9 @@ public class Sword : MonoBehaviour {
     }
 
     Vector2 rightStick = gamepad.rightStick.ReadValue();
+    float stickAngle = Vector3.SignedAngle(rightStick, Vector3.up, Vector3.back);
 
-    if (!slashTarget.HasValue) {
+    if (!slashing) {
       // Move sword toward the stick direction.
       transform.position = Vector3.SmoothDamp(
         transform.position, rightStick * scale, ref moveVelocity, moveDuration, maxMoveSpeed);
@@ -41,30 +45,42 @@ public class Sword : MonoBehaviour {
     else {
       // Move the sword toward the slash target. TODO: different speed values.
       transform.position = Vector3.SmoothDamp(
-        transform.position, slashTarget.Value, ref moveVelocity, moveDuration * 5, maxMoveSpeed);
+        transform.position, slashTarget, ref moveVelocity, moveDuration * 5, maxMoveSpeed);
 
-      if (Vector3.Distance(transform.position, slashTarget.Value) < .1f) {
-        slashTarget = null;
+      if (Vector3.Distance(transform.position, slashTarget) < .1f) {
+        slashing = false;
         cooldownRemaining = slashCooldown;
       }
     }
 
-    Vector3 currentAimDir = transform.rotation * Vector3.up;
-
-    // Aim the sword differently depending if the arm is at a slash or stab angle.
     bool aimForStab = stabAreas.Any(coll => coll.OverlapPoint(transform.position));
-    Vector3 targetAimDir = aimForStab ? target.position - transform.position
-      : transform.position - target.position;
 
-    Vector3 aimDir = Vector3.SmoothDamp(currentAimDir, targetAimDir, ref rotateVelocity,
-      rotateDuration, maxRotateSpeed);
-    transform.rotation = Quaternion.FromToRotation(Vector3.up, aimDir);
+    Quaternion targetRotation;
+    if (aimForStab && !slashing) {
+      // If sword is at stab angle, aim it directly at the target point.
+      targetRotation = Quaternion.FromToRotation(Vector3.up,
+        stabTarget.position - transform.position);
+    }
+    else {
+      if (!slashing) {
+        // If the sword is not slashing, aim it on an arc directly through the center.
+        targetRotation = SlashRotation(transform.position,
+          rightStick.normalized * scale, -rightStick.normalized * scale);
+      }
+      else {
+        // If the sword is slashing, aim it on an arc from the start to the target.
+        targetRotation = SlashRotation(transform.position, slashStart, slashTarget);
+      }
+    }
+
+    transform.rotation = Util.QuaternionSmoothDamp(
+      transform.rotation, targetRotation, ref rotateDeriv, rotateDuration, maxRotateSpeed);
 
     if (cooldownRemaining > 0) {
       cooldownRemaining -= Time.deltaTime;
     }
 
-    if (!slashTarget.HasValue && cooldownRemaining <= 0) {
+    if (!slashing && cooldownRemaining <= 0) {
       // If the sword is entering the slash area at a minimum speed, find and set the slash target.
       bool wasInSlashArea = inSlashArea;
       inSlashArea = slashArea.OverlapPoint(transform.position);
@@ -75,8 +91,32 @@ public class Sword : MonoBehaviour {
         // Find the target point on the opposite edge of the circle by passing it and going back.
         RaycastHit2D hit = Physics2D.Raycast(
           transform.position + moveVelocity.normalized * scale * 3, -moveVelocity);
+
+        slashing = true;
+        slashStart = transform.position;
         slashTarget = hit.point;
+        slashScale = Vector3.Distance(slashStart, slashTarget);
       }
     }
+  }
+
+  Quaternion SlashRotation(Vector2 position, Vector2 start, Vector2 target) {
+    float totalDistance = Vector2.Distance(start, target);
+    float distanceLeft = totalDistance > 0
+      ? Vector2.Distance(position, target) / totalDistance : .5f;
+
+    // Arc the sword toward the opponent when approaching the center, then away again.
+    float forwardAngle = Util.Map(1, 0, 5, 175, distanceLeft);
+
+    float stickAngle = Vector2.SignedAngle(position - target, Vector2.up);
+
+    // Tilt the sword up a bit when it's held low, to simulate a higher eye level.
+    float tiltUpAngle = (transform.position.y / scale - 1) * 22.5f;
+
+    return (
+      Quaternion.Euler(tiltUpAngle, 0, 0) *
+      Quaternion.Euler(0, 0, -stickAngle) *
+      Quaternion.Euler(forwardAngle, 0, 0)
+    );
   }
 }
--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -48,4 +48,34 @@ public class Util {
     value -= 2;
     return end * 0.5f * (value * value * value * value * value + 2) + start;
   }
+
+  public static float Vector3InverseLerp(Vector3 a, Vector3 b, Vector3 value) {
+    Vector3 AB = b - a;
+    Vector3 AV = value - a;
+    return Vector3.Dot(AV, AB) / Vector3.Dot(AB, AB);
+  }
+
+  public static Quaternion QuaternionSmoothDamp(
+    Quaternion current, Quaternion target, ref Quaternion deriv, float smoothTime, float maxSpeed
+  ) {
+		if (Time.deltaTime < Mathf.Epsilon) return current;
+		// account for double-cover
+		float sign = Quaternion.Dot(current, target) > 0f ? 1f : -1f;
+		// smooth damp (nlerp approx)
+		var Result = new Vector4(
+			Mathf.SmoothDamp(current.x, target.x * sign, ref deriv.x, smoothTime, maxSpeed),
+			Mathf.SmoothDamp(current.y, target.y * sign, ref deriv.y, smoothTime, maxSpeed),
+			Mathf.SmoothDamp(current.z, target.z * sign, ref deriv.z, smoothTime, maxSpeed),
+			Mathf.SmoothDamp(current.w, target.w * sign, ref deriv.w, smoothTime, maxSpeed)
+		).normalized;
+
+		// ensure deriv is tangent
+		var derivError = Vector4.Project(new Vector4(deriv.x, deriv.y, deriv.z, deriv.w), Result);
+		deriv.x -= derivError.x;
+		deriv.y -= derivError.y;
+		deriv.z -= derivError.z;
+		deriv.w -= derivError.w;
+
+		return new Quaternion(Result.x, Result.y, Result.z, Result.w);
+	}
 }
